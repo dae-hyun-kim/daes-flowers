cdcd6e06e8da6a183b8bb47be74ebf937c2ff448
dae-hyun-kim <daehk4@uci.edu>
2020-03-09 20:32:47 +0000

    Individual products rendered on page - Dae Kim

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
 export default class ProductListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   render() {
+    const product = this.props.props;
     return (
       <div className="card card-width">
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={product.image} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">Card Title</h5>
-          <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, possimus.</p>
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">{product.shortDescription}</p>
           <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
       </div>
--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductListItems from './product-list-item';
 
 export default class ProductList extends React.Component {
   constructor(props) {
@@ -6,10 +7,38 @@ export default class ProductList extends React.Component {
     this.state = {
       products: []
     };
+    this.getProducts = this.getProducts.bind(this);
+    this.createProduct = this.createProduct.bind(this);
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(response => {
+        return response.json();
+      }).then(result => {
+        this.setState({
+          products: result
+        });
+      });
   }
 
   componentDidMount() {
-
+    this.getProducts();
   }
 
+  createProduct() {
+    const productList = this.state.products;
+    const allProducts = productList.map((product, index) => {
+      return (<ProductListItems props={product} key={index}/>);
+    });
+    return allProducts;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.createProduct()}
+      </div>
+    );
+  }
 }
