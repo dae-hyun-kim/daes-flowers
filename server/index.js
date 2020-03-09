b6e09dae2f2fdd5db3cd9c077f2fcdce91d84d61
dae-hyun-kim <daehk4@uci.edu>
2020-03-09 18:32:28 +0000

    Fixed Get Method - Dae Kim

--- server/index.js
+++ server/index.js
@@ -14,13 +14,20 @@ app.use(sessionMiddleware);
 app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
+  db.query('select \'successfully connected\' as "message"')
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
+
+app.get('server/index.js', (req, res, next) => {
   const sql = `
   SELECT *
   FROM "products"
   `;
   db.query(sql)
-    .then(result => res.json(result.rows))
-    .catch(err => next(err));
+    .then(result => {
+      res.json(result.rows);
+    }).catch(err => next(err));
 });
 
 app.use('/api', (req, res, next) => {
