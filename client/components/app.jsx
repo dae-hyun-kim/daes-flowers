8d5ccb3e7c086699b0dc53f845a3a979a0503afe
dae-hyun-kim <daehk4@uci.edu>
2020-03-10 00:45:17 +0000

    Currently on Set View Method - Dae Kim

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      view: {
+        name: 'catalog',
+        params: {}
+      }
     };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
 
   componentDidMount() {
@@ -22,6 +36,9 @@ export default class App extends React.Component {
         </div>
         <div>
           <ProductList/>
+          <div>
+            <ProductDetails />
+          </div>
         </div>
       </div>
     );
