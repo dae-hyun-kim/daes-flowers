22239e7ca1a033f6942c9dc874dc8593b5854271
dae-hyun-kim <daehk4@uci.edu>
2020-04-12 04:58:04 +0000

    intro modal styling complete - Dae Kim

--- client/components/intro-modal.jsx
+++ client/components/intro-modal.jsx
@@ -4,14 +4,16 @@ export default class IntroModal extends React.Component {
   render() {
     return (
       <div className="intro-modal text-center">
-        <h2>Welcome</h2>
-        <h6>{'Dae\'s'} Flowers is a Full-Stack E-Commerce Content Management System using React.js and Node.js that was created
+        <div className="intro-modal-inner">
+          <h1>Welcome</h1>
+          <h6 className="mt-4">{'Dae\'s'} Flowers is a Full-Stack E-Commerce Content Management System using React.js and Node.js that was created
         strictly for demonstrational purposes.</h6>
-        <h6>This is not a real E-Commerce Website</h6>
-        <h6>By clicking the accept button below, you accept that no real purchases will be made and there will be no payment processing.
+          <h6 className="mt-4">This is not a real E-Commerce Website</h6>
+          <h6 className="mt-4">By clicking the accept button below, you accept that no real purchases will be made and there will be no payment processing.
           Actual personal information, such as real names, addresses, and/or credit card numbers should be used at checkout.
-        </h6>
-        <button className="btn btn-danger">Accept</button>
+          </h6>
+          <button className="btn btn-danger mt-3 w-75">Accept</button>
+        </div>
       </div>
     );
   }
