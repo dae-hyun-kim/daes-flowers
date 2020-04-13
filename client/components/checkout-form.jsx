e114307ad9ffc01acf2588596ba52c7506b1a7ce
dae-hyun-kim <daehk4@uci.edu>
2020-04-13 21:10:02 +0000

    form validation for credit card expiration year complete - Dae Kim

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -167,6 +167,10 @@ export default class CheckoutForm extends React.Component {
       this.setState({
         error: true
       });
+    } else if (!this.state.expireyear) {
+      this.setState({
+        error: true
+      });
     } else {
       const placeOrderMethod = this.props.placeOrder;
       this.setState({
@@ -351,7 +355,19 @@ export default class CheckoutForm extends React.Component {
 
               <label htmlFor="ccyear">
                 <h5>Year:</h5>
-                <input htmlFor="ccyear" type="number" onChange={this.handleExpireYear} value={this.state.expireyear}/>
+                <select htmlFor="ccyear" type="number" onChange={this.handleExpireYear} value={this.state.expireyear}>
+                  <option defaultValue hidden></option>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                  <option value="2022">2022</option>
+                  <option value="2023">2023</option>
+                  <option value="2024">2024</option>
+                  <option value="2025">2025</option>
+                  <option value="2026">2026</option>
+                  <option value="2027">2027</option>
+                  <option value="2028">2028</option>
+                  <option value="2029">2029</option>
+                </select>
               </label>
 
               <label htmlFor="cvv">
